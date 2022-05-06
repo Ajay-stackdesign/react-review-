b8c49982b289ab18b52ae8956d780c0c2984e943
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-05-06 18:07:44 +0530

    review

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import Review from "./Component/Review.js"
 
 function App() {
+  const [name, setName] = useState("")
+  const [number, setNumber] = useState("")
+  const [email, setEmail] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("aaa")
+    setName(window.prompt("name:" + "Please Confirm once agin", name))
+    setNumber(window.prompt("number:" + "Please Confirm once agin", number))
+    setEmail(window.prompt("email:" + "Please Confirm once agin", email))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit">submit</button>
+      </form>
+      <Review />
+
+
     </div>
   );
 }
